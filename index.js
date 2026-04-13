adc6e802ba05d82dfb6fc3663dae1ac71987c16b
Jane Akinyi Nyasoro <janenyasoro@gmail.com>
2026-04-13 11:24:08 +0300

    Javascript modules using spread operator

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
+//import datejs
+require("datejs");
+
+//combine multiple username arrays into a single merged array
+function combineUsers(...args){
+  const combinedObject = {
+    users: []
+  };
+  //loop through args and merge arrays using spread operator
+  for (let i = 0; i < args.length; i++){
+    combinedObject.users = [...combinedObject.users, ...args[i]];
+  }
+  //adding todays date using datejs
+  combinedObject.merge_date = new Date().toString("M/d/yyyy");
+  return combinedObject;
+}
 
 
 
